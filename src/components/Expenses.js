b777c2d1c32336b75c631754955cbaae87a13738
Gnössienne <91949315+hieutrung0451@users.noreply.github.com>
2022-10-23 15:44:11 +0700

    add

---
+++ src/components/Expenses.js
@@ -0,0 +1,20 @@
+import ExpenseItem from "./ExpenseItem";
+
+const Expenses = (props) => {
+  return (
+    <div className="expenses">
+      {props.items.map((expense) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Expenses;
